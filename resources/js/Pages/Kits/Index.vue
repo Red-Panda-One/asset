0968b694aa87b6c2018a8a00f7fbc34aa0a0f394
zurielbm <58864716+zurielbm@users.noreply.github.com>
2025-02-13 08:20:43 -0500

    Plural fro kit

--- resources/js/Pages/Kits/Index.vue
+++ resources/js/Pages/Kits/Index.vue
@@ -7,7 +7,7 @@ import Container from '@/Components/Container.vue'
     <AppLayout title="Kits">
         <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                Kit
+                Kits
             </h2>
         </template>
         <Container>
