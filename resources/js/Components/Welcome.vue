3c858f80a6bb146bd252617d387d798e947edb43
zurielbm <58864716+zurielbm@users.noreply.github.com>
2025-02-17 17:19:54 -0500

    Added verison

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -47,8 +47,10 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-6 stroke-gray-400">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
                     </svg>
-                    <h2 class="text-xl font-semibold text-gray-900 ms-3 dark:text-white">
-                        <a href="https://#">Version system - Seedling 0.0.0</a>
+                    <h2 class="flex text-xl font-semibold text-gray-900 ms-3 dark:text-white">
+                        <a href="https://#"><p>Version system </p>
+                        <p class="text-sm">Seedling 0.0.0</p>
+                        </a>
                     </h2>
                 </div>
 
